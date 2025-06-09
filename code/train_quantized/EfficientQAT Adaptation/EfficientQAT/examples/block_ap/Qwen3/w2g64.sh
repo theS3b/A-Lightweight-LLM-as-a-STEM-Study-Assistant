f655d1a4b822b223d49fe42f4220aa6a1a87cc09
Sébastien Delsad <seb.ge.ed@gmail.com>
2025-06-10 00:38:25 +0200

    Add EfficientQAT

---
+++ code/train_quantized/EfficientQAT Adaptation/EfficientQAT/examples/block_ap/Qwen3/w2g64.sh
@@ -0,0 +1,14 @@
+CUDA_VISIBLE_DEVICES=0 python main_block_ap.py \
+--model brygotti/MNLP_M2_mcqa_model  \
+--output_dir /scratch/izar/delsad/mnlp_cache/EfficientQAT/block_ap_log/Qwen3-w2g64 \
+--wbits 2 \
+--calib_dataset get_mnlp_randomsampling \
+--cache_dir /scratch/izar/delsad/mnlp_cache/EfficientQAT/cache \
+--group_size 64 \
+--quant_lr 1e-4 \
+--weight_lr 2e-5 \
+--training_seqlen 128 \
+--real_quant \
+# --eval_ppl \
+# --eval_tasks arc_easy,medmcqa \
+--save_quant_dir /scratch/izar/delsad/mnlp_cache/EfficientQAT/block_ap_models/Qwen3-w2g64
---
+++ code/train_quantized/EfficientQAT Adaptation/EfficientQAT/examples/model_transfer/efficientqat_to_gptq/Qwen3-w3g64.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+CUDA_VISIBLE_DEVICES=0 python  -m model_transfer.efficientqat_to_others \
+--model /scratch/izar/delsad/mnlp_cache/EfficientQAT/e2e-qp-output/Qwen3-w3g64/checkpoint-1000/ \
+--save_dir /scratch/izar/delsad/mnlp_cache/EfficientQAT/gptq/Qwen3-w3g64 \
+--wbits 3 \
+--group_size 64 \
+--target_format gptq
